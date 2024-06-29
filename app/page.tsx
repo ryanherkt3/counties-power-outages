932ae3afacb36d60e4c3a2a38a24b76b032cf0c7
ryanherkt3 <ryzys3@gmail.com>
2024-06-29 22:30:15 +1200

    Escape apostrophe

--- app/page.tsx
+++ app/page.tsx
@@ -22,7 +22,7 @@ export default function HomePage() {
             icon: 'BellAlertIcon',
             content: 'Subscribe to email alerts for notifications in your area on our Notifications page. All that is needed is your email address - no account required! You can also check for existing subscriptions on this page, and unsubscribe to any if you wish. In the future you may even be able to trigger an email alert with the click of a button, and subscribe to an alert on an Individual Outage page.'
         },
-    ]
+    ];
 
     return (
         <div className="flex flex-col gap-6 px-4 py-6 page-min-height">
@@ -30,7 +30,7 @@ export default function HomePage() {
             <div className="flex flex-col gap-3 text-lg">
                 <p>Welcome to the Counties Power Outages App, a React application built around the power outages in the Counties Manukau region in New Zealand.</p>    
                 <p>This app is not built by Counties Power, nor does it have any affiliation with the existing app they have built, which can be found <a className="visited:text-purple-500 hover:text-blue-500" href="https://app.countiespower.com/" target="_blank">here</a>.</p>    
-                <p>I built this app because I was not only unsatisfied with the existing app's UI for its planned outages section, but also because when there is a planned power outage in my neighbourhood, the letter that Counties Power sends often comes with a few days notice.</p>    
+                <p>I built this app because I was not only unsatisfied with the existing app&apos;s UI for its planned outages section, but also because when there is a planned power outage in my neighbourhood, the letter that Counties Power sends often comes with a few days notice.</p>    
                 <p>So, I decided to build an app that solve both these problems by creating an easy to navigate page showing the planned outages, and a page where it is possible to subscribe to email alerts for notifications in your area.</p>    
             </div>
             <div className="text-center text-3xl font-semibold">App Features</div>
