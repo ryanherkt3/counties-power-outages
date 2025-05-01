3e0177e8150ad290bb368eebe798f28ccb52df99
ryanherkt3 <ryzys3@gmail.com>
2025-05-01 20:07:23 +1200

    Fix redirect source & destination urls

--- next.config.mjs
+++ next.config.mjs
@@ -3,11 +3,8 @@ const nextConfig = {
     async rewrites() {
         return [
             {
-                source: '/api/:path*',
-                // destination: process.env.NODE_ENV === 'development'
-                //     ? 'http://127.0.0.1:8080/api/:path*'
-                //     : '/api/',
-                destination: 'https://api.integration.countiesenergy.co.nz/:path*',
+                source: '/api/getoutages',
+                destination: 'https://api.integration.countiesenergy.co.nz/user/v1.0/shutdowns',
             },
         ]
     },
