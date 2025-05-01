ac8a69adeb64ca251352ceb31f85020e244d1216
ryanherkt3 <ryzys3@gmail.com>
2025-05-01 17:40:01 +1200

    Fix skeleton nav bar

--- app/ui/skeletons/skeleton-nav-bar.tsx
+++ app/ui/skeletons/skeleton-nav-bar.tsx
@@ -1,30 +1,13 @@
 'use client';
 
-import { usePathname } from 'next/navigation';
-
 export default function SkeletonNavBar() {
-    const pathname = usePathname();
-
-    const links = [
-        {
-            href: '/outages',
-            linkName: 'Outages'
-        },
-        {
-            href: '/notifications',
-            linkName: 'Notifications'
-        }
-    ];
-
-    const activeLink = links.filter((items) => items.href === pathname)[0];
-
     const colourClasses = 'text-black border-gray-400 bg-white';
     const textClasses = 'text-2xl md:text-4xl font-semibold';
     const navClasses = 'flex justify-between items-center sticky top-0 h-20 px-6 py-4 border-b z-20';
 
     return (
         <div className={`${colourClasses} ${textClasses} ${navClasses}`}>
-            <span>{ activeLink ? activeLink.linkName : '404 Page' }</span>
+            <div className='text-xl font-semibold text-black hover:text-red-400'>Counties Power Outages App</div>
             <div className="shimmer max-md:w-8 md:w-[217px] h-8"></div>
         </div>
     );
