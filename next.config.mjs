6d6593640a940e7ed6a573e9868a4cf65fbfe086
ryanherkt3 <ryzys3@gmail.com>
2025-05-01 19:23:17 +1200

    Redirect test

--- next.config.mjs
+++ next.config.mjs
@@ -4,9 +4,10 @@ const nextConfig = {
         return [
             {
                 source: '/api/:path*',
-                destination: process.env.NODE_ENV === 'development'
-                    ? 'http://127.0.0.1:8080/api/:path*'
-                    : '/api/',
+                // destination: process.env.NODE_ENV === 'development'
+                //     ? 'http://127.0.0.1:8080/api/:path*'
+                //     : '/api/',
+                destination: 'https://api.integration.countiesenergy.co.nz/:path*',
             },
         ]
     },
