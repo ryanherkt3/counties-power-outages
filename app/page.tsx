745a1fcbfffdff101246907779cebd5d92a80afd
ryanherkt3 <ryanherkt@gmail.com>
2025-12-02 14:11:57 +1300

    Fix build issues

--- app/page.tsx
+++ app/page.tsx
@@ -2,14 +2,20 @@ import { getActiveOutages } from '../lib/utils';
 import { Metadata } from 'next';
 import OutagesList from '../components/outages-list';
 import { PromiseSearchParams } from '@/lib/definitions';
+import { notFound } from 'next/navigation';
 
 export const metadata: Metadata = {
     title: 'Outages | Counties Power Outages App',
 };
 
 export default async function OutagesPage(props: { searchParams: PromiseSearchParams }) {
-    const outages = await getActiveOutages();
     const searchParams = await props.searchParams;
+
+    if (!searchParams) {
+        return notFound();
+    }
+
+    const outages = await getActiveOutages();
 
     return (
         <OutagesList searchParams={searchParams} outages={outages} />
